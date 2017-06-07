088b201cbcbd139ddde5c52ae95091377b564edc
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-06-07 14:31:50 -0400

    Built a very simple ticking counter that add 1 every second to the intitial state set to 0

--- dev/index.jsx
+++ dev/index.jsx
@@ -3,71 +3,51 @@ import {render} from 'react-dom';
 
 var destination = document.querySelector('#container');
 
-class Square extends React.Component {
-  render () {
-    var squareStyle = {
-      height: '150px',
-      backgroundColor: this.props.color
-    };
-    return (
-      <div style={squareStyle}>
-      </div>
-    );
-  }
-};
-class Label extends React.Component {
-  render () {
-    var labelStyle = {
-      fontFamily: 'sans-serif',
-      fontWeight: 'bold',
-      textAlign: 'center'
-    };
-    return (
-      <p style={labelStyle}>{this.props.colorName}</p>
-    );
-  }
-};
-
-class Card extends React.Component {
-  render () {
-    var cardStyle = {
-      height: '200px',
-      width: '150px',
-      backgroundColor: '#FFF',
-      padding: '0px',
-      margin: '2em',
-      WebkitFilter: 'drop-shadow(0px 0px 5px #666)',
-      filter: 'drop-shadow(0px 0px 5px #666)'
-    };
-    return (
-      <div style = {cardStyle}>
-        <Square {...this.props}/>
-        <Label {...this.props}/>
-      </div>
-    );
-  }
-};
-
-class Inline extends React.Component {
-  render (){
-    var inlineStyle = {
-      display: "inline-block"
+class LightningCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timerTick = this.timerTick.bind(this);
+    this.state = {
+      strikes: 0
     }
+  }
+  timerTick(){
+    this.setState({
+      strikes: this.state.strikes + 1
+    });
+  }
+  componentDidMount(){
+    setInterval(this.timerTick, 1000);
+  }
+  render(){
     return (
-      <div style = {inlineStyle}>
-        <Card {...this.props}/>
+      <h1>{this.state.strikes}</h1>
+    );
+  };
+};
+
+class LightningCounterDisplay extends React.Component {
+  render(){
+    var divStyle = {
+      width: '250px',
+      textAlign: 'center',
+      backgroundColor: 'black',
+      padding: '40px',
+      fontFamily: 'sans-serif',
+      color: '#999',
+      borderRadius: '10px'
+      }
+    return (
+      <div style={divStyle}>
+      <LightningCounter/>
       </div>
     )
   }
 }
+
 render(
   <div>
-    <Inline color="#FFA737" colorName="Orange"/>
-    <Inline color="blue" colorName="Blue"/>
-    <Inline color="chartreuse" colorName="Chartreuse"/>
-    <Inline color="red" colorName="Red"/>
-    <Inline color="orange" colorName="Orange"/>
-    <Inline color="lightBlue" colorName="Light Blue"/>
+    <LightningCounterDisplay/>
   </div>,
   destination
 );
